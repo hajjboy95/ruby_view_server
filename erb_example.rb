8b9cc4dbbb9fa2afbfc7bcede3d0e38f17aa8e0e
hajjboy95 <gizmo-boy95@hotmail.com>
2015-05-18 14:17:45 +0100

    added more examples

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,18 @@
 require 'erb'
 
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
-puts template.result(binding)
+numbers = [32,213,1,4,5,74]
+
+largest_number = numbers.max
+smallest_number = numbers.min
+average_number = numbers.inject { |sum,elem| sum + elem }.to_f
+
+template1 = ERB.new "The value of x is: <%= x %>"
+template2 = ERB.new "the largest value of the numbers array is <%= largest_number%>"
+template3 = ERB.new "the smallest value of the numbers array is <%= smallest_number%>"
+template4 = ERB.new "the avarage value of the numbers array is <%= average_number%>"
+
+puts template1.result(binding)
+puts template2.result(binding)
+puts template3.result(binding)
+puts template4.result(binding)
